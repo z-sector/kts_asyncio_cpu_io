f1bc706af2bd565d095acd7dc45bf31268f7fea3
George Ramaniuk <george.ramaniuk@gmail.com>
2021-10-25 12:12:51 +0300

    added io seq

--- io_ops/seq.py
+++ io_ops/seq.py
@@ -1,7 +1,23 @@
-def main():
-    # todo
-    pass
+import time
+from typing import Any, List, Dict
+
+import requests
+
+
+def main() -> List[Dict[str, Any]]:
+    res = []
+    for _ in range(10):
+        res.append(
+            requests.get('https://api.covidtracking.com/v1/us/current.json').json()
+        )
+
+    return res
 
 
 if __name__ == "__main__":
+    s = time.perf_counter()
     main()
+    elapsed = time.perf_counter() - s
+    print(f'=====================================================================')
+    print(f'{__file__} executed in {elapsed:0.4f} seconds.')
+    print(f'=====================================================================')
