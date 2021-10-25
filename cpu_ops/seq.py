f51046c7cbbd2573ad536ad0100c8297a2c6cf5d
George Ramaniuk <george.ramaniuk@gmail.com>
2021-10-25 12:50:08 +0300

    added cpu seq

--- cpu_ops/seq.py
+++ cpu_ops/seq.py
@@ -1,7 +1,21 @@
-def main():
-    # todo
-    pass
+import time
+from typing import List
+
+from utils import countdown
+
+
+def main() -> List[int]:
+    res = []
+    for _ in range(10):
+        res.append(countdown())
+
+    return res
 
 
 if __name__ == "__main__":
+    s = time.perf_counter()
     main()
+    elapsed = time.perf_counter() - s
+    print(f'=====================================================================')
+    print(f'{__file__} executed in {elapsed:0.4f} seconds.')
+    print(f'=====================================================================')
---
+++ cpu_ops/utils.py
@@ -0,0 +1,6 @@
+def countdown() -> int:
+    i = 0
+    while i < 5_000_000:
+        i += 1
+
+    return i
